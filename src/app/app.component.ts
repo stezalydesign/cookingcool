8b3bf1ef51693817033127047b12b964502963ca
Igor Alves <bonfarj@gmail.com>
2018-02-01 23:31:04 +0100

    Removing unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <app-ingredients></app-ingredients>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle = 'Cool Cooking';
 }
